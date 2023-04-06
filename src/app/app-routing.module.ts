7cb0f9a72c08db11fc01eae9524e7bd3264b3760
Christos Marroquin <christos.marroquin@hotmail.com>
2023-04-06 17:16:40 -0600

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ApiPageComponent } from './pages/api-page/api-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: DashboardComponent,
+    path: '',
+    children: [
+      {
+        component: HomePageComponent,
+        path: '',
+      },
+      {
+        component: ApiPageComponent,
+        path: 'api',
+      },
+      {
+        component: ContactPageComponent,
+        path: 'contact',
+      },
+      {
+        component: AboutPageComponent,
+        path: 'about',
+      },
+      {
+        component: AboutPageComponent,
+        path: 'faqs',
+      },
+    ],
+  },
+  {
+    component: LoginPageComponent,
+    path: 'login',
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material/material.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ApiPageComponent } from './pages/api-page/api-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+// Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ErrorPageComponent,
+    ContactPageComponent,
+    ApiPageComponent,
+    AboutPageComponent,
+    NavComponent,
+    DashboardComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
